9a8077b09876c69382ef5454c46229f6aca0f640
vincent <cj194832@163.com>
2019-10-15 20:53:40 +0800

    Deployed 9558ad4 with MkDocs version: 1.0.4

--- static/js/baidu-static.js
+++ static/js/baidu-static.js
@@ -1,7 +1,7 @@
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?5bbafbb1b5e47d6c8da68e6875345893";
+  hm.src = "https://hm.baidu.com/hm.js?fe82e127fb0154453a060524fa48f4f6";
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
